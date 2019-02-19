c3123e52c48b2ed74804687bb9f54e280f0c7deb
Dan Sanduleac <dsanduleac@palantir.com>
2019-02-19 21:11:26 +0000

    simplify test

--- src/test/groovy/com/palantir/gradle/versions/VersionsLockPluginIntegrationSpec.groovy
+++ src/test/groovy/com/palantir/gradle/versions/VersionsLockPluginIntegrationSpec.groovy
@@ -390,9 +390,6 @@ class VersionsLockPluginIntegrationSpec extends IntegrationTestKitSpec {
         settingsFile << "include ':foo:bar'"
         new File(subdir, 'build.gradle') << """
             apply plugin: 'java-library'
-            dependencies {
-                implementation 'org.slf4j:slf4j-api:1.7.24'
-            }
         """.stripIndent()
 
         expect:
