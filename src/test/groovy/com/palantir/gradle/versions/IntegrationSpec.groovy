d902a29a0e3238ea665ff9d2e2b647a34620ce4a
Dan Sanduleac <dsanduleac@palantir.com>
2020-02-24 17:45:13 +0000

    stop delegating, use new traits exposed in nebula-test

--- src/test/groovy/com/palantir/gradle/versions/IntegrationSpec.groovy
+++ src/test/groovy/com/palantir/gradle/versions/IntegrationSpec.groovy
@@ -17,8 +17,7 @@
 package com.palantir.gradle.versions
 
 import groovy.transform.CompileStatic
-import nebula.test.BaseIntegrationSpec
-import nebula.test.IntegrationTestKitSpec
+import nebula.test.IntegrationTestKitBase
 import nebula.test.dependencies.DependencyGraph
 import nebula.test.dependencies.GradleDependencyGenerator
 import org.gradle.testkit.runner.GradleRunner
@@ -27,14 +26,10 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.TestInfo
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.io.TempDir
-import spock.lang.Specification
 
 @CompileStatic
 @ExtendWith(ParameterizedClass)
-class IntegrationSpec {
-    public static final String testKitDirProperty = "org.gradle.testkit.dir"
-    @Delegate(excludeTypes = [ Specification ], interfaces = true, allNames = true)
-    private ExposedIntegrationTestKitSpec testKit = new ExposedIntegrationTestKitSpec()
+class IntegrationSpec implements IntegrationTestKitBase {
 
     @TempDir
     public File gradleTestKitDir
@@ -49,76 +44,34 @@ class IntegrationSpec {
 
     @BeforeEach
     final void beforeEachMain(TestInfo testInfo) {
-        testKit.gradleVersion = gradleVersion
-        testKit.testInfo = testInfo
+        def testName = (testInfo.displayName + " " + testInfo.testMethod.get().name)
+                .replace('()', "")
+        IntegrationTestKitBase.super.initialize(testInfo.testClass.get(), testName)
 
-        // Need to unwind spock's order of 'setup' methods
-        // First call the one on BaseIntegrationSpec, then the one on IntegrationTestKitSpec
-        testKit.baseSetup()
-        invokeHiddenMethod(testKit, IntegrationTestKitSpec, 'setup')
+        IntegrationTestKitBase.super.gradleVersion = gradleVersion
 
-        testKit.keepFiles = true
+        keepFiles = true
         // testKit.debug = true // TODO cannot do this when running in parallel
         // Ends up in: org.gradle.testkit.runner.internal.ToolingApiGradleExecutor.buildConnector -> embedded
-        testKit.settingsFile.createNewFile()
     }
 
     @AfterEach
     final void afterEachMain() {
-        invokeHiddenMethod(testKit, IntegrationTestKitSpec, 'cleanup')
-        System.clearProperty(testKitDirProperty)
+        cleanup()
     }
 
-    private static <T> void invokeHiddenMethod(T obj, Class<T> clazz, String methodName) {
-        def baseSetup = clazz.declaredMethods.find { it.name == methodName }
-        baseSetup.setAccessible(true)
-        baseSetup.invoke(obj)
+    @Override
+    GradleRunner createRunner(String... tasks) {
+        // Very important: need each test kit to use a different location
+        // Currently it sets it to a static location via org.gradle.testkit.runner.internal.DefaultGradleRunner.calculateTestKitDirProvider
+        return IntegrationTestKitBase.super.createRunner(tasks).withTestKitDir(gradleTestKitDir)
     }
 
     @CompileStatic
     protected File generateMavenRepo(String... graph) {
         DependencyGraph dependencyGraph = new DependencyGraph(graph)
         GradleDependencyGenerator generator = new GradleDependencyGenerator(
-                dependencyGraph, new File(testKit.projectDir, "build/testrepogen").toString())
+                dependencyGraph, new File(projectDir, "build/testrepogen").toString())
         return generator.generateTestMavenRepo()
-    }
-
-    class ExposedIntegrationTestKitSpec extends IntegrationTestKitSpec {
-        private TestInfo testInfo
-
-        @Override
-        File createFile(String path, File baseDir = getProjectDir()) {
-            super.createFile(path, baseDir)
-        }
-
-        @Override
-        File file(String path, File baseDir = getProjectDir()) {
-            return super.file(path, baseDir)
-        }
-
-        @Override
-        File directory(String path, File baseDir = getProjectDir()) {
-            return super.directory(path, baseDir)
-        }
-
-        @Override
-        GradleRunner createRunner(String... tasks) {
-            // Very important: need each test kit to use a different location
-            // Currently it sets it to a static location via org.gradle.testkit.runner.internal.DefaultGradleRunner.calculateTestKitDirProvider
-            return super.createRunner(tasks).withTestKitDir(gradleTestKitDir)
-        }
-
-        /** Supersedes {@link BaseIntegrationSpec#setup} to use testInfo directly. */
-        def baseSetup() {
-            def sanitizedTestName = (testInfo.displayName + " " + testInfo.testMethod.get().name)
-                    .replace('()', "")
-                    .replaceAll(/\W+/, '-')
-            projectDir = new File("build/nebulatest/${testInfo.testClass.get().canonicalName}/${sanitizedTestName}").absoluteFile
-            if (projectDir.exists()) {
-                projectDir.deleteDir()
-            }
-            projectDir.mkdirs()
-            moduleName = findModuleName()
-        }
     }
 }
