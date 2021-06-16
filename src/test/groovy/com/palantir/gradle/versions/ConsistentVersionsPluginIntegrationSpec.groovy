e48739b4b79284f54e5b58c10b8547265f6db947
Callum Rogers <callumr@palantir.com>
2021-06-16 13:48:56 +0100

    Add test

--- src/test/groovy/com/palantir/gradle/versions/ConsistentVersionsPluginIntegrationSpec.groovy
+++ src/test/groovy/com/palantir/gradle/versions/ConsistentVersionsPluginIntegrationSpec.groovy
@@ -62,12 +62,27 @@ class ConsistentVersionsPluginIntegrationSpec extends IntegrationSpec {
         """.stripIndent()
     }
 
-    def '#gradleVersionNumber: can write locks'() {
+    def '#gradleVersionNumber: can write locks using --write-locks'() {
         setup:
         gradleVersion = gradleVersionNumber
 
         when:
         runTasks('--write-locks')
+
+        then:
+        new File(projectDir, "versions.lock").exists()
+        runTasks('resolveConfigurations')
+
+        where:
+        gradleVersionNumber << GRADLE_VERSIONS
+    }
+
+    def '#gradleVersionNumber: can write locks using writeVersionsLock'() {
+        setup:
+        gradleVersion = gradleVersionNumber
+
+        when:
+        runTasks('writeVersionsLock')
 
         then:
         new File(projectDir, "versions.lock").exists()
