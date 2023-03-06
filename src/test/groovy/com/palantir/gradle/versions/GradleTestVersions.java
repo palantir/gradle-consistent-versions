9e4a55e35b3c88f067e38dff421e0e9f6e1cee93
Callum Rogers <callumr@palantir.com>
2023-03-06 16:07:34 +0000

    Test with the right versions

--- src/test/groovy/com/palantir/gradle/versions/GradleTestVersions.java
+++ src/test/groovy/com/palantir/gradle/versions/GradleTestVersions.java
@@ -23,5 +23,5 @@ public final class GradleTestVersions {
     private GradleTestVersions() {}
 
     public static final List<String> GRADLE_VERSIONS =
-            ImmutableList.of(VersionsLockPlugin.MINIMUM_GRADLE_VERSION.getVersion(), "6.9", "7.1.1");
+            ImmutableList.of(VersionsLockPlugin.MINIMUM_GRADLE_VERSION.getVersion(), "7.6", "7.6.1");
 }
