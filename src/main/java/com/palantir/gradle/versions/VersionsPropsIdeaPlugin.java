7da19cc277ff927438c4457598f17e88490ad442
FinlayRJW <88376465+FinlayRJW@users.noreply.github.com>
2024-10-04 15:50:48 +0100

    Plugin to create a maven repo list in the .ideas folder (#1233)

---
+++ src/main/java/com/palantir/gradle/versions/VersionsPropsIdeaPlugin.java
@@ -0,0 +1,54 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.gradle.versions;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import org.gradle.StartParameter;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.repositories.MavenArtifactRepository;
+import org.gradle.api.tasks.TaskProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class VersionsPropsIdeaPlugin implements Plugin<Project> {
+    private static final Logger log = LoggerFactory.getLogger(VersionsPropsIdeaPlugin.class);
+
+    @Override
+    public void apply(Project project) {
+
+        if (!Boolean.getBoolean("idea.active")) {
+            return;
+        }
+
+        TaskProvider<GenerateMavenRepositoriesTask> writeMavenRepositories = project.getTasks()
+                .register("writeMavenRepositories", GenerateMavenRepositoriesTask.class, task -> {
+                    task.getMavenRepositories().set(project.provider(() -> project.getRepositories().stream()
+                            .filter(repo -> repo instanceof MavenArtifactRepository)
+                            .map(repo ->
+                                    ((MavenArtifactRepository) repo).getUrl().toString())
+                            .map(url -> url.endsWith("/") ? url : url + "/")
+                            .collect(Collectors.toSet())));
+                });
+
+        StartParameter startParameter = project.getGradle().getStartParameter();
+        List<String> taskNames = startParameter.getTaskNames();
+        taskNames.add(String.format(":%s", writeMavenRepositories.getName()));
+        startParameter.setTaskNames(taskNames);
+    }
+}
---
+++ src/test/groovy/com/palantir/gradle/versions/VersionPropsIdeaPluginIntegrationSpec.groovy
@@ -0,0 +1,81 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.gradle.versions
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import com.fasterxml.jackson.datatype.guava.GuavaModule
+import nebula.test.IntegrationSpec
+import spock.util.environment.RestoreSystemProperties
+
+import java.util.stream.Collectors
+
+class VersionPropsIdeaPluginIntegrationSpec extends IntegrationSpec {
+
+    def setup() {
+        //language=gradle
+        buildFile << """
+            repositories {
+                maven {
+                    url 'https://test'
+                }
+                maven {
+                    url 'https://demo/'
+                }
+                mavenCentral() { metadataSources { mavenPom(); ignoreGradleMetadataRedirection() } }
+            }
+
+            apply plugin: 'com.palantir.version-props-idea'
+            apply plugin: 'idea'
+        """.stripIndent(true)
+
+        def ideaDir = new File(projectDir, '.idea')
+        ideaDir.mkdirs()
+    }
+
+    def "plugin creates gcv-maven-repositories.xml file in .idea folder"() {
+        when: 'we run the first time'
+        runTasksSuccessfully('-Didea.active=true')
+
+        then: 'we generate the correct config'
+        def repoFile = new File(projectDir, '.idea/gcv-maven-repositories.xml')
+        repoFile.exists()
+
+        // language=xml
+        def expectedXml = '''
+            <repositories>
+              <repository url="https://test/"/>
+              <repository url="https://repo.maven.apache.org/maven2/"/>
+              <repository url="https://demo/"/>
+            </repositories>
+        '''.stripIndent(true).trim()
+
+        def projectNode = new XmlParser().parse(repoFile)
+        nodeToXmlString(projectNode) == expectedXml
+
+        when: 'we run the second time'
+        def secondRun = runTasksSuccessfully('-Didea.active=true')
+
+        then: "if nothing has changed, the task is then up-to-date"
+        secondRun.wasUpToDate(":writeMavenRepositories")
+    }
+
+    private static String nodeToXmlString(debugRunConf) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream()
+        new XmlNodePrinter(new PrintWriter(baos)).print(debugRunConf)
+        return baos.toString().trim()
+    }
+}
